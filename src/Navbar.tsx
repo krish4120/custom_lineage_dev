c7ca8fed4bdf5a31efbe39e19d96e1777ee33cf6
krish4120 <krishna.kumar@gmail.com>
2024-09-13 17:53:09 +0530

    Update the Navbar design

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -19,51 +19,25 @@ const Navbar: React.FC<NavbarProps> = ({
   onDownload,
   onSearch,
   onNext,
-  onReset,
-  databases = [],
-  schemas = [],
-  tables = []
+  onReset
 }) => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         <img src="../custom_lineage_dev/Bi3-Logo.svg" alt="Logo" />
       </div>
-      <div className="navbar-dropdowns">
-        <select>
-          <option value="">Select Database</option>
-          {databases.map((db) => (
-            <option key={db} value={db}>{db}</option>
-          ))}
-        </select>
-        <select>
-          <option value="">Select Schema</option>
-          {schemas.map((schema) => (
-            <option key={schema} value={schema}>{schema}</option>
-          ))}
-        </select>
-        <select>
-          <option value="">Select Table</option>
-          {tables.map((table) => (
-            <option key={table} value={table}>{table}</option>
-          ))}
-        </select>
-        <select>
-          <option value="">Select Type</option>
-          <option value="parent">Parent</option>
-          <option value="child">Child</option>
-        </select>
-        <button onClick={onReset}>Reset</button>
-      </div>
       <div className="navbar-search"> {/* Place search box before the icons */}
         <SearchBox onSearch={onSearch} onNext={onNext} />
+      </div>
+      <div className="navbar-dropdowns">
+        <button className="navbar-logo" onClick={onReset}><img src="../custom_lineage_dev/refresh.png" alt="Refresh" /></button>
       </div>
       <div className="navbar-icons">
         <label className="toggle-switch">
           <input type="checkbox" defaultChecked={true} onChange={onExpandCollapseToggle} />
           <span className="slider"></span>
         </label>
-        <button onClick={onDownload}>Download</button>
+        <button className="navbar-button"onClick={onDownload}>Download</button>
         <div className="navbar-profile">
           <img src="../custom_lineage_dev/user.png" alt="Profile" />
         </div>
